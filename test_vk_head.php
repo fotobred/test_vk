7515fbad7bd769b4172e7ea83cbe966a83f3be73
Troshin Aleksei <wwwwalks@ya.ru>
2023-03-05 16:08:19 +0300

    first vers.
    first ver.

---
+++ test_vk_head.php
@@ -0,0 +1,18 @@
+<?
+/*
+*	test_vk_
+*	шапка страницы
+*/
+echo '
+<!doctype html>
+<html>
+<head data-live-domain="walks.ru">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>test_vk </title>
+</head> 
+<body style="font-family:Lucida Console; color: #555; font-size: 22px ">
+<h3>это Тест на стажировку в VK </h3>
+ кто-то считает, что это задача на позицию "джуниор"...<br>
+ моя очередная безнадежная попытка,<br> но когда это меня останавливало? <br>';
+?>
